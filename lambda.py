a68c1b6aeeadf92c3ada6eb8e75fc9624e95389e
Radha <radhabhoyar07@gmail.com>
2026-04-02 12:37:13 +0530

    added cases

--- lambda.py
+++ lambda.py
@@ -1,3 +1,16 @@
 students = [('alice',90), ('bob', 100),('marie',73)]
 students.sort(key=lambda x:x[1])
 print(students)
+
+
+def classify(x):
+    if x>3:
+        return "pos"
+    else:
+        return "neg"
+print (classify(8))
+
+
+classify = lambda x:  "pos" if x>5 else "neg"
+print (classify(2))
+print (classify(9))
