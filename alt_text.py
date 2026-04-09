539b120bdd9dc21724df029cd0a976bc82304db1
Radha <radhabhoyar07@gmail.com>
2026-04-09 13:49:18 +0530

    write_read_filei/o eg

---
+++ alt_text.py
@@ -0,0 +1,16 @@
+with open('plants.txt', 'w') as f: # opens AND will auto-close when block ends
+    
+    
+    f.write('photosynthesis\n')
+    f.write('oxygen\n')
+    f.write('soil\n')
+    
+print("Write done")
+
+
+    
+print('file saved and created')
+
+#with open('abc.txt','w') as f:
+#f = open('xyz.txt','w')
+
