0b9ada892871805f8a03aaba05e1e16841c006c5
Radha <radhabhoyar07@gmail.com>
2026-04-26 20:53:13 +0530

    class with no __str__ or __repr__

--- fundamentals/dunder.py
+++ fundamentals/dunder.py
@@ -9,4 +9,7 @@ msg = chat('user','whats rag in one sentence?', tokens=12)
 print(msg)
 print(repr(msg))
 
-
+#A class with no __str__ or __repr__
+#output
+#<__main__.chat object at 0x0000022D3CC46F90>
+#<__main__.chat object at 0x0000022D3CC46F90>
