b259c592ca2bcf099669ea127da0bb4da2e5a46f
Radha <radhabhoyar07@gmail.com>
2026-04-26 21:37:08 +0530

    class with __str__ __repr__

--- fundamentals/dunder.py
+++ fundamentals/dunder.py
@@ -4,12 +4,26 @@ class chat:
         self.content = content
         self.tokens = tokens
         
-msg = chat('user','whats rag in one sentence?', tokens=12)
+    def __str__(self) -> str:
+        preview = self.content[:50] + '...' if len(self.content) > 50 else self.content
+        return f'[{self.role.upper()}] {preview}'
+
+msg = chat('user', 'what is rag?', tokens=12)
+msg_long = chat('assist', 'rag stands for retrieval augmented gen and it fetches relvant docs first')
 
 print(msg)
-print(repr(msg))
+print(msg_long)
+print(f"message: {msg}")
 
-#A class with no __str__ or __repr__
-#output
-#<__main__.chat object at 0x0000022D3CC46F90>
-#<__main__.chat object at 0x0000022D3CC46F90>
+'''msg = chat('user','whats rag in one sentence?', tokens=12)
+
+print(msg)
+print(repr(msg))'''
+
+    #A class with no __str__ or __repr__
+    #output
+    #<__main__.chat object at 0x0000022D3CC46F90>
+    #<__main__.chat object at 0x0000022D3CC46F90>
+
+
+   
